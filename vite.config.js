42757233d4cbd9f0ed253f0d070b4ee52c010971
JD Lien <jd@jdlien.com>
2025-04-16 19:13:56 -0700

    Formatting

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,19 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ['@jdlien/validator-utils'],
-  },
+  optimizeDeps: { include: ['@jdlien/validator-utils'] },
   build: {
-    lib: {
-      entry: 'index.ts',
-      name: 'Validator',
-      fileName: 'validator',
-    },
+    lib: { entry: 'index.ts', name: 'Validator', fileName: 'validator' },
     rollupOptions: {
       input: 'index.ts',
-      output: {
-        entryFileNames: 'validator.js',
-        manualChunks: undefined,
-      },
+      output: { entryFileNames: 'validator.js', manualChunks: undefined },
     },
   },
-  ts: {
-    declaration: true,
-    declarationDir: 'dist',
-    declarationMap: true,
-  },
+  ts: { declaration: true, declarationDir: 'dist', declarationMap: true },
   test: {
     environment: 'jsdom',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    },
+    coverage: { reporter: ['text', 'json', 'html'] },
     threads: false, // suppresses errors from canvas when starting tests
-    deps: {
-      inline: ['@jdlien/validator-utils'],
-    },
-  }
+    deps: { inline: ['@jdlien/validator-utils'] },
+  },
 })
